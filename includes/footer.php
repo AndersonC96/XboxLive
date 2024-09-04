bd4150b6a66203648a73b07e1e3eaf9310a0bd6c
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-04 17:20:47 -0300

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+        <footer class="bg-gray-800 text-white py-4 mt-auto">
+            <div class="text-center">
+                <p>&copy; 2024 Xbox. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    </body>
+</html>
