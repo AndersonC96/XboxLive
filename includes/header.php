0e658398010180fdc540cbff727bcd94dcbdd093
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-04 17:35:32 -0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -7,26 +7,3 @@
         <title>Xbox</title>
     </head>
     <body class="bg-gray-100">
-        <nav class="bg-gray-800 p-4">
-            <div class="flex justify-between items-center">
-                <div class="flex space-x-4">
-                    <a href="#" class="text-white">Conta</a>
-                    <a href="#" class="text-white">Conquistas</a>
-                    <a href="#" class="text-white">Atividade</a>
-                    <a href="#" class="text-white">Clubes</a>
-                    <a href="#" class="text-white">Conversas</a>
-                    <a href="#" class="text-white">DVR</a>
-                    <a href="#" class="text-white">Amigos</a>
-                    <a href="#" class="text-white">Gamepass</a>
-                    <a href="#" class="text-white">Loja</a>
-                    <a href="#" class="text-white">Jogador</a>
-                    <a href="#" class="text-white">Sessão</a>
-                    <a href="#" class="text-white">Grupo</a>
-                </div>
-                <div class="flex items-center space-x-4">
-                    <input type="text" placeholder="Buscar" class="px-4 py-2 rounded-lg">
-                    <img src="profile_pic_url" alt="Profile" class="w-10 h-10 rounded-full">
-                    <a href="logout.php" class="text-white">Sair</a>
-                </div>
-            </div>
-        </nav>
