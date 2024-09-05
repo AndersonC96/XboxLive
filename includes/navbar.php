d757a57472003a4198a51b8f53ca7232ff8d5f2b
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-05 17:05:59 -0300

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,34 @@
+<?php
+    session_start();
+    require '../config/db.php';
+    require '../config/api.php';
+    $user_id = $_SESSION['user_id'];
+    $stmt = $pdo->prepare("SELECT xuid FROM users WHERE id = :user_id");
+    $stmt->execute(['user_id' => $user_id]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($user && $user['xuid']) {
+        $xuid = $user['xuid'];
+        $endpoint = "account";
+        $response = openXBLRequest($endpoint);
+        if (isset($response['profileUsers'][0]['settings'])) {
+            $settings = $response['profileUsers'][0]['settings'];
+            foreach ($settings as $setting) {
+                if ($setting['id'] === 'GameDisplayPicRaw') {
+                    $gamerpic = $setting['value'];
+                }
+                if ($setting['id'] === 'Gamertag') {
+                    $gamertag = $setting['value'];
+                }
+            }
+        } else {
+            $gamerpic = 'default_avatar.jpg';
+            $gamertag = 'Usuário';
+        }
+    } else {
+        $gamerpic = 'default_avatar.jpg';
+        $gamertag = 'Usuário';
+    }
+?>
 <nav class="bg-gray-800 p-4">
     <div class="flex justify-between items-center">
         <div class="flex items-center space-x-4">
@@ -19,7 +50,7 @@
         </div>
         <div class="flex items-center space-x-4">
             <input type="text" placeholder="Buscar" class="px-4 py-2 rounded-lg">
-            <img src="profile_pic_url" alt="Profile" class="w-10 h-10 rounded-full">
+            <img src="<?php echo $gamerpic; ?>" alt="Profile" class="w-10 h-10 rounded-full">
             <a href="logout.php" class="text-white">Sair</a>
         </div>
     </div>
