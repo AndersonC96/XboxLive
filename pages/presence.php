a89b3b0fd30fafb200cf7db84807897c9a482069
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-06 12:10:34 -0300

    Update presence.php

--- pages/presence.php
+++ pages/presence.php
@@ -23,15 +23,24 @@
             $presenceData = 'Desconhecido';
         }
     }
+    function getDeviceIcon($deviceType) {
+        switch ($deviceType) {
+            case 'Scarlett':
+                return '<img src="../img/xboxseries.png" alt="Xbox Series" class="inline-block w-6 h-6">';
+            case 'Android':
+                return '<img src="../assets/android.svg" alt="Android" class="inline-block w-6 h-6">';
+            default:
+                return $deviceType;
+        }
+    }
 ?>
 <div class="container mx-auto mt-10">
-    <h1 class="text-3xl font-bold mb-6">Presença</h1>
     <div class="bg-gray-800 p-4 rounded-lg shadow-md">
         <p><strong>Status</strong>: <?php echo $presenceData; ?></p>
         <?php if ($lastSeenDetails): ?>
             <div class="mt-4">
-                <p><strong>Último Acesso</strong></p>
-                <p><b>Dispositivo</b>: <?php echo $lastSeenDetails['deviceType']; ?></p>
+                <p><strong>Visto por último</strong></p>
+                <p><b>Dispositivo</b>: <?php echo getDeviceIcon($lastSeenDetails['deviceType']); ?></p>
                 <p><b>Jogo / Aplicativo</b>: <?php echo $lastSeenDetails['titleName']; ?></p>
                 <p><b>Última vez online</b>: <?php echo date('d/m/Y H:i:s', strtotime($lastSeenDetails['timestamp'])); ?></p>
             </div>
