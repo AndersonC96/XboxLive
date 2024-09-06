24316f0b7238254f2b6afb703a30148cf7c729e6
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-06 12:31:37 -0300

    Update alerts.php

--- pages/alerts.php
+++ pages/alerts.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    include ('../includes/header.php');
-    include ('../includes/navbar.php');
+    include('../includes/header.php');
+    include('../includes/navbar.php');
     require_once '../config/api.php';
     $endpoint = "alerts";
     $response = openXBLRequest($endpoint);
@@ -10,16 +10,18 @@
 <div class="container mx-auto mt-10">
     <h1 class="text-3xl font-bold mb-6">Alertas</h1>
     <?php if (!empty($alerts)): ?>
-    <ul class="bg-gray-800 p-4 rounded-lg shadow-md">
-        <?php foreach ($alerts as $alert): ?>
-        <li class="border-b border-gray-700 py-2">
-            <p><strong><?php echo $alert['title']; ?></strong></p>
-            <p><?php echo $alert['message']; ?></p>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+        <ul class="bg-gray-800 p-4 rounded-lg shadow-md">
+            <?php foreach ($alerts as $alert): ?>
+                <li class="border-b border-gray-700 py-2">
+                    <p><strong><?php echo $alert['action']; ?></strong></p>
+                    <p><?php echo $alert['actorGamertag']; ?></p>
+                    <p><?php echo isset($alert['message']) ? $alert['message'] : 'Sem mensagem'; ?></p>
+                    <p><?php echo date('d/m/Y H:i:s', strtotime($alert['timestamp'])); ?></p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     <?php else: ?>
-    <p>Não há alertas disponíveis no momento.</p>
+        <p>Não há alertas disponíveis no momento.</p>
     <?php endif; ?>
 </div>
 <?php include('../includes/footer.php'); ?>
