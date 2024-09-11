a5fc661c5394b4589b0e8fd7995d2e04e14ac2a7
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-11 18:12:30 -0300

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -104,7 +104,6 @@
                     <a href="../pages/promocao.php" class="block px-4 py-2 hover:bg-gray-600">Promoção</a>
                 </div>
             </div>
-            <a href="#" class="text-white">Loja</a>
             <a href="#" class="text-white">Jogador</a>
         </div>
         <form action="search.php" method="GET" class="flex">
