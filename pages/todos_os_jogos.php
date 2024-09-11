0aba26b26f60bd9a1c08b2a207c2d8c05f24f048
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-11 16:57:25 -0300

    Update todos_os_jogos.php

--- pages/todos_os_jogos.php
+++ pages/todos_os_jogos.php
@@ -34,9 +34,24 @@
             <?php foreach ($response['Products'] as $product) : ?>
                 <li class="mb-4">
                     <div class="flex items-center space-x-4">
-                        <img src="<?php echo $product['LocalizedProperties'][0]['Images'][0]['Uri']; ?>" alt="Imagem do jogo" class="w-16 h-16 rounded-full">
+                        <?php
+                            $boxArtImage = null;
+                            if (isset($product['LocalizedProperties'][0]['Images']) && is_array($product['LocalizedProperties'][0]['Images'])) {
+                                foreach ($product['LocalizedProperties'][0]['Images'] as $image) {
+                                    if ($image['ImagePurpose'] === 'BoxArt') {
+                                        $boxArtImage = $image['Uri'];
+                                        break;
+                                    }
+                                }
+                            }
+                        ?>
+                        <?php if ($boxArtImage): ?>
+                            <img src="<?php echo 'https:' . $boxArtImage; ?>" alt="Imagem do jogo" class="w-16 h-16 rounded-full">
+                        <?php else: ?>
+                            <img src="../img/placeholder.png" alt="Imagem não disponível" class="w-16 h-16 rounded-full">
+                        <?php endif; ?>
                         <div>
-                            <p class="text-xl"><?php echo htmlspecialchars($product['LocalizedProperties'][0]['ProductTitle']); ?></p>
+                            <p class="text-xl"><?php echo htmlspecialchars($product['LocalizedProperties'][0]['ProductTitle'] ?? 'Título não disponível'); ?></p>
                             <p class="text-sm text-gray-400">
                                 Preço:
                                 <?php
@@ -47,7 +62,11 @@
                                     }
                                 ?>
                             </p>
-                            <p class="text-sm text-gray-400">Descrição: <?php echo htmlspecialchars($product['LocalizedProperties'][0]['ShortDescription']); ?></p>
+                            <p class="text-sm text-gray-400">Descrição: <?php echo htmlspecialchars($product['LocalizedProperties'][0]['ProductDescription'] ?? 'Descrição não disponível'); ?></p>
+                            <p class="text-sm text-gray-400">Desenvolvedora: <?php echo htmlspecialchars($product['LocalizedProperties'][0]['DeveloperName'] ?? 'Desconhecida'); ?></p>
+                            <p class="text-sm text-gray-400">Publisher: <?php echo htmlspecialchars($product['LocalizedProperties'][0]['PublisherName'] ?? 'Desconhecida'); ?></p>
+                            <p class="text-sm text-gray-400">Franquia: <?php echo htmlspecialchars($product['LocalizedProperties'][0]['Franchises'][0] ?? 'Não disponível'); ?></p>
+                            <p class="text-sm text-gray-400">Categoria: <?php echo htmlspecialchars($product['Properties']['Category'] ?? 'Não disponível'); ?></p>
                         </div>
                     </div>
                 </li>
