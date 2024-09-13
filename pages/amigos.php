8b1c86dc5b03ac6f5df795776138e6e4ac05ecb3
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-13 18:02:46 -0300

    Update amigos.php

--- pages/amigos.php
+++ pages/amigos.php
@@ -29,28 +29,29 @@
 <div class="container mx-auto p-4">
     <h1 class="text-3xl mb-4">Lista de Amigos</h1>
     <?php if (!empty($friends)) : ?>
-        <ul>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <?php foreach ($friends as $friend) : ?>
-                <li class="mb-4">
-                    <div class="flex items-center space-x-4">
+                <div class="bg-gray-800 text-white rounded-lg shadow-lg overflow-hidden">
+                    <div class="flex">
                         <?php
                             $avatar = !empty($friend['displayPicRaw']) ? $friend['displayPicRaw'] : '../img/default_avatar.jpg';
                             $addedDate = new DateTime($friend['addedDateTimeUtc']);
                             $formattedDate = $addedDate->format('d/m/Y');
                         ?>
-                        <img src="<?php echo $avatar; ?>" alt="Avatar de <?php echo $friend['displayName']; ?>" class="w-16 h-16 rounded-full">
-                        <div>
-                            <p class="text-xl"><?php echo htmlspecialchars($friend['gamertag']); ?></p>
+                        <img src="<?php echo $avatar; ?>" alt="Avatar de <?php echo $friend['displayName']; ?>" class="w-1/3 h-auto object-cover">
+                        <div class="p-4 flex-1">
+                            <h2 class="text-2xl font-bold mb-2"><?php echo htmlspecialchars($friend['gamertag']); ?></h2>
                             <p class="text-sm text-gray-400">Amigo desde: <?php echo $formattedDate; ?></p>
-                            <p class="text-sm text-gray-400">Gamerscore: <?php echo number_format($friend['gamerScore'], 0, ',', '.'); ?>
-                                <img src="../img/gs.png" alt="Gamerscore Icon" class="inline-block w-4 h-4">
+                            <p class="text-sm text-gray-400 flex items-center">
+                                Gamerscore: <?php echo number_format($friend['gamerScore'], 0, ',', '.'); ?>
+                                <img src="../img/gs.png" alt="Gamerscore Icon" class="w-4 h-4 ml-2">
                             </p>
-                            <p class="text-sm text-gray-400"><?php echo $friend['presenceText']; ?></p>
+                            <p class="text-xs text-gray-400 mt-4"><?php echo $friend['presenceText']; ?></p>
                         </div>
                     </div>
-                </li>
+                </div>
             <?php endforeach; ?>
-        </ul>
+        </div>
     <?php else : ?>
         <p>Você não tem amigos adicionados no momento.</p>
     <?php endif; ?>
