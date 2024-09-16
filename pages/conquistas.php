94cfcf9618fc710abe85a2f031c5e6bee63b3ac5
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-09-16 17:03:54 -0300

    Update conquistas.php

--- pages/conquistas.php
+++ pages/conquistas.php
@@ -28,22 +28,33 @@
     function getDeviceIcon($deviceType) {
         switch ($deviceType) {
             case 'XboxSeries':
-                return '<img src="../img/xboxseries.png" alt="Xbox Series" class="inline-block w-6 h-6">';
+                return '<img src="../img/xboxseries.png" alt="Xbox Series" width="62,5" height="62,5">';  // Ajuste de tamanho
             case 'PC':
-                return '<img src="../img/windows.png" alt="PC" class="inline-block w-6 h-6">';
+                return '<img src="../img/windows.png" alt="PC" width="62,5" height="62,5">';
             case 'XboxOne':
-                return '<img src="../img/xboxone.png" alt="Xbox One" class="inline-block w-6 h-6">';
+                return '<img src="../img/xboxone.png" alt="Xbox One" width="62,5" height="62,5">';
             case 'Win32':
-                return '<img src="../img/windows.png" alt="PC" class="inline-block w-6 h-6">';
+                return '<img src="../img/windows.png" alt="PC" width="62,5" height="62,5">';
             case 'Mobile':
-                return '<img src="../img/windowsphone.webp" alt="Windows Phone" class="inline-block w-6 h-6">';
+                return '<img src="../img/windowsphone.webp" alt="Windows Phone" width="62,5" height="62,5">';
             case 'Xbox360':
-                return '<img src="../img/xbox360.png" alt="Xbox 360" class="inline-block w-6 h-6">';
+                return '<img src="../img/xbox360.png" alt="Xbox 360" width="62,5" height="62,5">';
             default:
                 return $deviceType;
+        }
     }
-}
-    $items_per_page = 10;
+    function getBoxArt($game) {
+        $defaultImage = '../img/default_game.jpg';
+        if (isset($game['images'])) {
+            foreach ($game['images'] as $image) {
+                if ($image['type'] === 'BoxArt') {
+                    return $image['url'];
+                }
+            }
+        }
+        return $defaultImage;
+    }
+    $items_per_page = 12;
     $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
     $offset = ($page - 1) * $items_per_page;
     $total_games = count($games);
@@ -52,37 +63,37 @@
 <div class="container mx-auto p-4">
     <h1 class="text-3xl mb-4">Conquistas</h1>
     <?php if (!empty($games_to_display)) : ?>
-        <ul>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
             <?php foreach ($games_to_display as $game) : ?>
-                <li class="mb-4">
-                    <div class="flex items-center space-x-4">
-                        <img src="<?php echo $game['displayImage']; ?>" alt="Imagem do jogo" class="w-16 h-16 rounded-full">
-                        <div>
-                            <p class="text-xl"><?php echo htmlspecialchars($game['name']); ?></p>
-                            <p class="text-sm text-gray-400">Gamerscore: <?php echo number_format($game['achievement']['currentGamerscore'], 0, ',', '.'); ?> <img src="../img/gs.png" alt="Gamerscore Icon" class="inline-block w-4 h-4"></p>
-                            <p class="text-sm text-gray-400">Progresso: <?php echo $game['achievement']['progressPercentage']; ?>%</p>
-                            <p class="text-sm text-gray-400">Plataformas:
-                                <?php
+                <div class="bg-gray-800 p-4 rounded-lg shadow-md flex">
+                    <div class="relative w-1/3">
+                        <img src="<?php echo getBoxArt($game); ?>" alt="Imagem do jogo" class="object-cover rounded-l-md h-full">
+                    </div>
+                    <div class="w-2/3 p-4">
+                        <p class="text-xl font-bold text-white"><?php echo htmlspecialchars($game['name']); ?></p>
+                        <p class="text-sm text-gray-400">Gamerscore: <?php echo number_format($game['achievement']['currentGamerscore'], 0, ',', '.'); ?> <img src="../img/gs.png" alt="Gamerscore Icon" class="inline-block w-4 h-4"></p>
+                        <p class="text-sm text-gray-400">Progresso: <?php echo $game['achievement']['progressPercentage']; ?>%</p>
+                        <p class="text-sm text-gray-400 flex items-center space-x-2">Plataformas:
+                            <?php
                                 if (isset($game['devices']) && is_array($game['devices'])) {
                                     foreach ($game['devices'] as $device) {
-                                        echo getDeviceIcon($device) . ' ';
+                                        echo getDeviceIcon($device);
                                     }
                                 } else {
                                     echo 'Plataforma não disponível';
                                 }
-                                ?>
-                            </p>
-                            <p class="text-sm text-gray-400">Jogado pela última vez:
-                                <?php
+                            ?>
+                        </p>
+                        <p class="text-sm text-gray-400">Jogado pela última vez:
+                            <?php
                                 $lastPlayed = new DateTime($game['titleHistory']['lastTimePlayed']);
                                 echo $lastPlayed->format('d/m/Y');
-                                ?>
-                            </p>
-                        </div>
+                            ?>
+                        </p>
                     </div>
-                </li>
+                </div>
             <?php endforeach; ?>
-        </ul>
+        </div>
         <div class="mt-4">
             <?php if ($page > 1): ?>
                 <a href="?page=<?php echo $page - 1; ?>" class="bg-gray-800 text-white px-4 py-2 rounded">Anterior</a>
