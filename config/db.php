a19b12f3c2ec960df048fb08329f2566fec12380
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-11-21 13:07:11 -0300

    Update dotenv path and DB password handling
    Changed Dotenv loading path to parent directory and improved database password retrieval to support both DB_PASSWORD and DB_PASS environment variables.

--- config/db.php
+++ config/db.php
@@ -1,11 +1,11 @@
 <?php
     require_once __DIR__ . '/../vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
     $dotenv->load();
     $host = $_ENV['DB_HOST'];
     $db = $_ENV['DB_NAME'];
     $user = $_ENV['DB_USER'];
-    $pass = $_ENV['DB_PASS'];
+    $pass = isset($_ENV['DB_PASSWORD']) ? $_ENV['DB_PASSWORD'] : (isset($_ENV['DB_PASS']) ? $_ENV['DB_PASS'] : '');
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
