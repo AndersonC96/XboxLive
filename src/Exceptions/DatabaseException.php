731bf102ea95b869a4fd003c923239bc47fd4bc1
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2026-04-12 00:30:47 -0300

    Split exceptions into separate files
    Extract individual exception classes into their own files. Added AuthException.php and XblApiException.php (with strict_types), and renamed AppExceptions.php to DatabaseException.php while removing the duplicated Auth/XblApi classes from it. This clarifies responsibilities and improves file organization for exception types.

--- src/Exceptions/DatabaseException.php
+++ src/Exceptions/DatabaseException.php
@@ -5,6 +5,4 @@ namespace Anderson\XboxLive\Exceptions;
 
 use Exception;
 
-class XblApiException extends Exception {}
 class DatabaseException extends Exception {}
-class AuthException extends Exception {}
---
+++ src/Exceptions/XblApiException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace Anderson\XboxLive\Exceptions;
+
+use Exception;
+
+class XblApiException extends Exception {}
